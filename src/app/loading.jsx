f39153356d4d8b04d18e40b200d58abfe4d5e0f8
sharat <sharat@farefirst.com>
2024-03-01 12:09:53 +0530

    Added the white background for each button and used path

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,6 +1,4 @@
 const Loading = () => {
-    return(
-        <div>Loading</div>
-    )
-}
+  return <div>Loading</div>;
+};
 export default Loading;
--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,41 +1,45 @@
-import Link from "next/link"
-import styles from "./links.module.css"
+import Link from "next/link";
+import styles from "./links.module.css";
+import NavLink from "./navLink/NavLink";
 
 const Links = () => {
-const links = [
-{
-    title : "HomePage",
-    path : "/",
-},
-{
-    title : "About",
-    path : "/about",
-},
-{
-    title : "Contact",
-    path : "/contact",
-},
-{
-    title : "Admin",
-    path : "/admin",
-},
-{
-    title : "Blog",
-    path : "/blog",
-},
-{
-    title : "Login",
-    path : "/login",
-}
-];
+  const links = [
+    {
+      title: "HomePage",
+      path: "/",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+    {
+      title: "Admin",
+      path: "/admin",
+    },
+    {
+      title: "Blog",
+      path: "/blog",
+    },
+    {
+      title: "Login",
+      path: "/login",
+    },
+  ];
 
-    return (
-       <div className={styles.links}>
-        {links.map(link =>(
-            <Link href={link.path} key={link.title}>{link.title}</Link>
-        ))}
-       </div>
-    )
-}
+  return (
+    <div className={styles.links}>
+      {links.map((link) => (
+        <NavLink item={link} key={link.title} />
+        // <Link href={link.path} key={link.title}>
+        //   {link.title}
+        // </Link>
+      ))}
+    </div>
+  );
+};
 
-export default Links
+export default Links;
