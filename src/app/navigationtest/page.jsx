2afd41c14e9eef7525dd7f71e0604aca2839e2c0
sharat <sharat@farefirst.com>
2024-03-06 17:41:13 +0530

    Navigation of page params

--- src/app/navigationtest/page.jsx
+++ src/app/navigationtest/page.jsx
@@ -1,29 +1,32 @@
-"use client"
-import Link from "next/link"
-import { usePathname, useRouter } from "next/navigation"
+"use client";
+import Link from "next/link";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
+const NavigationTestPage = () => {
+    //CLIENT SIDE NAVIGATION
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParam = useSearchParams();
 
+  const q = searchParam.get("q");
 
-const NavigationTestPage = () =>{
+  console.log(q);
+  const handleClick = () => {
+    console.log("clicked");
+    // router.push("/")
+    // router.replace("/")
+    router.refresh("/");
+    // router.back()
+    // router.forward()
+  };
 
-    const router = useRouter()
-    const pathname = usePathname()
-    console.log(pathname);
-    const handleClick = () =>{
-        console.log("clicked");
-        // router.push("/")
-        // router.replace("/")
-        router.refresh("/")
-        // router.back()
-        // router.forward()
-     }
-     
-
-    return(
-        <div>
-        <Link href="/" prefetch= {false}>Click Here</Link>
-        <button onClick={handleClick}>Write and Redirect</button>
-        </div>
-    )
-}
-export default NavigationTestPage
+  return (
+    <div>
+      <Link href="/" prefetch={false}>
+        Click Here
+      </Link>
+      <button onClick={handleClick}>Write and Redirect</button>
+    </div>
+  );
+};
+export default NavigationTestPage;
