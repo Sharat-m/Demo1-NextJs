960775fb80d23ee8623007b7246488e02f747316
sharat <sharat@farefirst.com>
2024-03-07 11:05:16 +0530

    Fetching the data from the API and checking in the console

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,22 @@
 import PostCard from "@/components/postCard/postCard";
 import styles from "./blog.module.css";
 
-const BlogPage = ({params,searchParams}) => {
+
+// Fetch the data from the API
+const getData = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+  return res.json();
+};
+
+const BlogPage = async () => {
   // console.log(params);
-  console.log(searchParams);
+  // console.log(searchParams);
+  const posts = await getData();
+  console.log(posts);
   return (
     <div className={styles.container}>
       <div className={styles.post}>
