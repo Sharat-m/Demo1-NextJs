bf3b43a2ed380d3d5c91370e31db295fd712977b
sharat <sharat@farefirst.com>
2024-03-14 12:23:29 +0530

    Server Action step-4:Adding Post and Deleting Post

--- src/app/serveractiontest/page.jsx
+++ src/app/serveractiontest/page.jsx
@@ -1,25 +1,32 @@
-import addPost from "@/lib/action"
+import addPost, { deletePost } from "@/lib/action";
 
+const ServerActionTestPage = () => {
+  return (
+    <div>
+      <form action={addPost}>
+        <input type="text" placeholder="Blog Title" name="title" />
+        <br />
+        <br />
+        <input type="text" placeholder="Blog Description" name="desc" />
+        <br />
+        <br />
+        <input type="text" placeholder="slug" name="slug" />
+        <br />
+        <br />
+        <input type="text" placeholder="Blog userId" name="userId" />
+        <br />
+        <br />
+        <input type="text" placeholder="Image Url" name="img" />
+        <br />
+        <br />
+        <button>Create Blog</button>
+      </form>
 
-
-const ServerActionTestPage = () =>{
-    return (
-        <div>
-            <form action={addPost}>
-                <input type="text" placeholder="Blog Title" name="title" />
-                <br /><br />
-                <input type="text" placeholder="Blog Description" name="desc"/>
-                <br /><br />
-                <input type="text" placeholder="slug" name="slug"/>
-                <br /><br />
-                <input type="text" placeholder="Blog userId" name="userId"/>
-                <br /><br />
-                <input type="text" placeholder="Image Url" name="img" />
-                <br />
-                <br />
-                <button>Create Blog</button>
-            </form>
-        </div>
-    )
-}
-export default ServerActionTestPage
+      <form action={deletePost}>
+        <input type="text" placeholder="Post id" name="id" />
+        <button>Delete Blog</button>
+      </form>
+    </div>
+  );
+};
+export default ServerActionTestPage;
--- src/lib/action.js
+++ src/lib/action.js
@@ -1,15 +1,17 @@
+"use server";
+import { revalidatePath } from "next/cache";
 import { Post } from "./models";
 import { connectToDb } from "./utils";
 
-const addPost = async (formData) =>{
-  "use server";
+export const addPost = async (formData) => {
+ 
 
   // const title =formData.get("title");
   // const desc =formData.get("desc");
   // const slug =formData.get("slug");
   // const userId =formData.get("userId");
   const { title, desc, slug, userId, img } = Object.fromEntries(formData);
-  console.log(title, desc, slug, userId,img);
+  console.log(title, desc, slug, userId, img);
   try {
     connectToDb();
     const newPost = new Post({
@@ -21,9 +23,24 @@ const addPost = async (formData) =>{
     });
     await newPost.save();
     console.log("saved to db");
+    revalidatePath("/blog")
   } catch (error) {
     console.log(error);
-    return {error : "Something went Wrong"};
+    return { error: "Something went Wrong" };
   }
-}
-export default addPost;
+};
+
+export const deletePost = async (formData) => {
+  
+  const { id } = Object.fromEntries(formData);
+  console.log(id);
+  try {
+    connectToDb();
+    await Post.findByIdAndDelete(id);
+    console.log("Deleted the Blog from DataBase");
+    revalidatePath("/blog")
+  } catch (error) {
+    console.log(error);
+    return { error: "Something went Wrong" };
+  }
+};
