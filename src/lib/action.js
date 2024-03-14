0cd10c81f0ef818ba58a5e4cf8eb598e1e233421
sharat <sharat@farefirst.com>
2024-03-14 10:41:43 +0530

    Server action Step-2

--- src/lib/action.js
+++ src/lib/action.js
@@ -1,11 +1,11 @@
 const addPost = async (formData) =>{
   "use server";
 
-  const title =formData.get("title");
-  const desc =formData.get("desc");
-  const slug =formData.get("slug");
-  const userId =formData.get("userId");
-
+  // const title =formData.get("title");
+  // const desc =formData.get("desc");
+  // const slug =formData.get("slug");
+  // const userId =formData.get("userId");
+  const { title, desc, slug, userId } = Object.fromEntries(formData);
   console.log(title, desc, slug, userId);
 }
 export default addPost;
